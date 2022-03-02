92bb29024d3823d4c3aa337bd8f904b52159861a
FHallengreen <fred70p9@stud.kea.dk>
2022-03-02 14:31:41 +0100

    texttoNumber added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,21 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
   Scanner scan = new Scanner(System.in);
+  final String ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+  char[] alphabetToChars = ALPHABET.toCharArray();
 
-  public int homeScreen() {
-    System.out.printf("Type 1 for Caesar Algorithm \n" +
-        "Type 2 for Vigenère algorithm \n");
-    int input = 0;
+  int input = 0;
 
+  public void checkInput() {
+    input = scan.nextInt();
     while (input != 1 && input != 2) { // Created a while loop using try / if / catch to check if user is typing correct information.
-      System.out.println("Please enter 1 for Caesar or 2 for Vigenère: ");
+      System.out.println("Please enter 1 or 2: ");
       try {
         input = Integer.parseInt(scan.nextLine());
         if (input != 1 && input != 2) {
@@ -22,29 +25,41 @@ public class Main {
         System.out.println("Input is invalid!"); // if user types other than numbers, it will return this message.
       }
     }
-    if (input == 1) { // Following will now direct user to next method based on their decision or return them homeScreen()
-      return caesar();
-    }
-      return vigenere();
+    System.out.println("You entered: " + input);
   }
 
-  public int caesar() {
+  public void homePage() {
+    System.out.printf("Type 1 for Caesar Algorithm \n" +
+        "Type 2 for Vigenère algorithm \n");
+    checkInput(); // will go to the checkInput() method to assign input a new value & make sure return value is either 1 & 2.
+    if (input == 1) { // Following will now direct user to next method based on their decision or return them homeScreen()
+      caesarPage();
+    }
+    vigenerePage();
+  }
+
+  public void caesarPage() { // The Caesar landing page which uses same loop and if statements to direct user to next page.
     System.out.println("You have chosen the Caesar Algorithm. Please select if you want to encrypt or decrypt a message:");
     System.out.printf("Type 1 to encrypt a message\n" +
         "Type 2 to decrypt a message\n");
-    int input = scan.nextInt();
+    checkInput();
     if (input == 1) {
-      return caesarEncrypt();
-    } else if (input == 2) {
-      return caesarDecryption();
+      caesarEncrypt();
+    } else {
+      caesarDecryption();
     }
-    System.out.println("You entered something else, please try again...");
-    return caesar();
   }
 
-  public int vigenere() {
-    //FILL
-    return 0;
+  public void vigenerePage() {
+    System.out.println("You have chosen the Vigenère Algorithm. Please select if you want to encrypt or decrypt a message:");
+    System.out.printf("Type 1 to encrypt a message\n" +
+        "Type 2 to decrypt a message\n");
+    checkInput();
+    if (input == 1) {
+      vigenereEncrypt();
+    } else {
+      vigenereDecryption();
+    }
   }
 
   public int caesarEncrypt() {
@@ -55,11 +70,38 @@ public class Main {
     return 0;
   }
 
-  public static void main(String[] args) {
+  public int vigenereEncrypt() {
+    return 0;
+  }
 
-    System.out.println("Welcome to my encryption / decryption tool");
-    System.out.println("Please select which algorithm you want to use:");
+  public int vigenereDecryption() {
+    return 0;
+  }
+
+  public void textToNumber(String inputText) {
+    inputText = inputText.toUpperCase();
+    int[] charPosition = new int [inputText.length()];
+
+    for (int i = 0; i < inputText.length(); i++) {
+     charPosition[i] = ALPHABET.indexOf(inputText.charAt(i)); // The character matching alphabet will be stored in charPos array.
+      }
+  }
+
+  public char numberToText(int inputNumber) {
+    char number = ALPHABET.charAt(inputNumber);
+    System.out.println(number);
+    return number;
+  }
+
+
+  public static void main(String[] args) {
     Main obj = new Main();
-    obj.homeScreen();
+
+ /*   System.out.println("Welcome to my encryption / decryption tool");
+    System.out.println("Please select which algorithm you want to use:");
+
+    obj.homePage();*/
+    String inputText = "Frederik";
+    obj.textToNumber(inputText.toUpperCase());
   }
 }
