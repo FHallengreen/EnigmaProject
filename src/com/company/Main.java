c156e910ebda37a4b4d352ed19f36b1b1cd23353
FHallengreen <fred70p9@stud.kea.dk>
2022-03-02 11:43:05 +0100

    Homescreen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class Main {
+  Scanner scan = new Scanner(System.in);
+
+  public int homeScreen() {
+    System.out.println("Please select which algorithm you want to use:");
+    System.out.printf("Type 1 for Caesar Algorithm \n" +
+        "Type 2 for Vigenère algorithm \n");
+    int press = scan.nextInt();
+    if (press == 1) {
+      return caesar();}
+    else if (press == 2) {
+      return vigenere();
     }
+    return homeScreen();
+  }
+
+  public int caesar() {
+    //FILL
+    return 0;
+  }
+
+  public int vigenere() {
+    //FILL
+    return 0;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println("Welcome to my encryption / decryption tool");
+    Main obj = new Main();
+    obj.homeScreen();
+  }
 }
