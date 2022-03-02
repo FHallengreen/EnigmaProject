49c88ca18b54a0c76009340cd71ce79347783409
FHallengreen <fred70p9@stud.kea.dk>
2022-03-02 12:19:37 +0100

    Added loop if input is incorrect

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,31 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
   Scanner scan = new Scanner(System.in);
 
   public int homeScreen() {
-    System.out.println("Please select which algorithm you want to use:");
     System.out.printf("Type 1 for Caesar Algorithm \n" +
         "Type 2 for Vigenère algorithm \n");
-    int input = scan.nextInt();
-    if (input == 1) {
-      return caesar();}
-    else if (input == 2) {
-      return vigenere();
+    int input = 0;
+
+    while (input != 1 && input != 2) { // Created a while loop using try / if / catch to check if user is typing correct information.
+      System.out.println("Please enter 1 for Caesar or 2 for Vigenère: ");
+      try {
+        input = Integer.parseInt(scan.nextLine());
+        if (input != 1 && input != 2) {
+          System.out.println("Input is not in choices!"); // If user types other numbers, it will return this message.
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("Input is invalid!"); // if user types other than numbers, it will return this message.
+      }
     }
-    return homeScreen();
+    if (input == 1) { // Following will now direct user to next method based on their decision or return them homeScreen()
+      return caesar();
+    }
+      return vigenere();
   }
 
   public int caesar() {
@@ -24,10 +34,11 @@ public class Main {
         "Type 2 to decrypt a message\n");
     int input = scan.nextInt();
     if (input == 1) {
-      return caesarEncrypt();}
-    else if (input == 2) {
+      return caesarEncrypt();
+    } else if (input == 2) {
       return caesarDecryption();
     }
+    System.out.println("You entered something else, please try again...");
     return caesar();
   }
 
@@ -36,17 +47,18 @@ public class Main {
     return 0;
   }
 
-  public int caesarEncrypt () {
+  public int caesarEncrypt() {
     return 0;
   }
 
-  public int caesarDecryption (){
+  public int caesarDecryption() {
     return 0;
   }
 
   public static void main(String[] args) {
 
     System.out.println("Welcome to my encryption / decryption tool");
+    System.out.println("Please select which algorithm you want to use:");
     Main obj = new Main();
     obj.homeScreen();
   }
